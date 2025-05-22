b2763a45e3e1a57cec1e33b72ec85cba08a0d643
hiddevb <hiddevanbavel@hotmail.com>
2025-05-22 09:23:00 +0200

    refactor: add README and lint script

---
+++ README.md
@@ -0,0 +1,12 @@
+## Moleneind site
+
+Static site for Stal Moleneind.
+
+Depends only on `PostCSS` to tranform future-standards-compliant CSS into styling with wider support
+
+### TODO
+ - [ ] Update images
+ - [ ] Update text
+ - [ ] modernize css toolchain with plain, future-standards-compliant CSS
+    - postcss with a basic script https://github.com/postcss/postcss?tab=readme-ov-file#npm-scripts
+ - [ ] get rid of gulp
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,62 +1,72 @@
 /* Hamburger Menu */
-const hamburger = document.getElementById('hamburger');
-const headerNav = document.getElementById('navigation');
+
+const hamburger = document.getElementById("hamburger");
+const headerNav = document.getElementById("navigation");
 
 let active = false;
 
-hamburger.onclick = function () {
-    active = !active;
-    if (active) {
-        hamburger.className = 'hamburger active';
-        headerNav.className = 'vertical';
-    } else {
-        hamburger.className = 'hamburger';
-        headerNav.className = '';
-    }
+hamburger.onclick = () => {
+	active = !active;
+	if (active) {
+		hamburger.className = "hamburger active";
+		headerNav.className = "vertical";
+	} else {
+		hamburger.className = "hamburger";
+		headerNav.className = "";
+	}
 };
 
-const headerIcon = document.getElementById('header-icon');
+const headerIcon = document.getElementById("header-icon");
 
-let showOnScroll = function () {
-    let y = window.scrollY;
-    if (y <= 50) {
-        headerIcon.className = 'header-icon';
-    } else {
-        headerIcon.className = 'header-icon hide';
-    }
+const showOnScroll = () => {
+	if (window.scrollY <= 50) {
+		headerIcon.className = "header-icon";
+	} else {
+		headerIcon.className = "header-icon hide";
+	}
 };
-window.addEventListener('scroll', showOnScroll);
+window.addEventListener("scroll", showOnScroll);
 
 /* JSMail */
 
 function generateMessage(message, phone) {
-    return message + "\n\n telefoon: " + phone;
+	return `${message}\n\n telefoon: ${phone}`;
 }
 
-(function () {
-    emailjs.init('user_tsBv3qPDJaxkkm7K9BsQi');
+(() => {
+	emailjs.init("user_tsBv3qPDJaxkkm7K9BsQi");
 
-    window.onload = function () {
-        document.getElementById('contact-form').addEventListener('submit', function (event) {
-            event.preventDefault();
-            emailjs.send("hiddevbavel_gmail_com", "template_V9HUOUNS", {
-                "from_name": document.querySelector("#user_name").value,
-                "message": generateMessage(
-                    document.querySelector("#message").value,
-                    document.querySelector("#user_phone").value),
-                "from_mail": document.querySelector("#user_mail").value,
-            }).then(function (response) {
-                document.querySelector("#contact-form").classList.add("hidden")
-                document.querySelector("#succes-message").classList.remove("hidden")
+	window.onload = () => {
+		document
+			.getElementById("contact-form")
+			.addEventListener("submit", (event) => {
+				event.preventDefault();
+				emailjs
+					.send("hiddevbavel_gmail_com", "template_V9HUOUNS", {
+						from_name: document.querySelector("#user_name").value,
+						message: generateMessage(
+							document.querySelector("#message").value,
+							document.querySelector("#user_phone").value,
+						),
+						from_mail: document.querySelector("#user_mail").value,
+					})
+					.then(
+						(response) => {
+							document.querySelector("#contact-form").classList.add("hidden");
+							document
+								.querySelector("#succes-message")
+								.classList.remove("hidden");
 
-                const snack = document.querySelector("#snackbar");
-                snack.className = "show";
-                setTimeout(function () {
-                    snack.className = snack.className.replace("show", "");
-                }, 5000);
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
-        });
-    }
+							const snack = document.querySelector("#snackbar");
+							snack.className = "show";
+							setTimeout(() => {
+								snack.className = snack.className.replace("show", "");
+							}, 5000);
+						},
+						(error) => {
+							console.log("FAILED...", error);
+						},
+					);
+			});
+	};
 })();

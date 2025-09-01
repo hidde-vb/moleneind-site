a8cf897a4370c661a71cefb2946c37316ffdcaf7
hiddevb <hiddevanbavel@hotmail.com>
2025-09-01 21:05:12 +0200

    fix(emailjs): fix and update email js

--- src/script.js
+++ src/script.js
@@ -1,74 +1,69 @@
-import './css/main.css';
+import emailjs from "@emailjs/browser";
+import "./css/main.css";
 
 /* Hamburger Menu */
 
-const hamburger = document.getElementById('hamburger');
-const headerNav = document.getElementById('navigation');
+const hamburger = document.getElementById("hamburger");
+const headerNav = document.getElementById("navigation");
 
 let active = false;
 
 hamburger.onclick = () => {
-	active = !active;
-	if (active) {
-		hamburger.className = 'hamburger active';
-		headerNav.className = 'vertical';
-	} else {
-		hamburger.className = 'hamburger';
-		headerNav.className = '';
-	}
+  active = !active;
+  if (active) {
+    hamburger.className = "hamburger active";
+    headerNav.className = "vertical";
+  } else {
+    hamburger.className = "hamburger";
+    headerNav.className = "";
+  }
 };
 
-const headerIcon = document.getElementById('header-icon');
+const headerIcon = document.getElementById("header-icon");
 
 const showOnScroll = () => {
-	if (window.scrollY <= 50) {
-		headerIcon.className = 'header-icon';
-	} else {
-		headerIcon.className = 'header-icon hide';
-	}
+  if (window.scrollY <= 50) {
+    headerIcon.className = "header-icon";
+  } else {
+    headerIcon.className = "header-icon hide";
+  }
 };
-window.addEventListener('scroll', showOnScroll);
+
+window.addEventListener("scroll", showOnScroll);
 
 /* JSMail */
 
-function generateMessage(message, phone) {
-	return `${message}\n\n telefoon: ${phone}`;
-}
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  try {
+    await emailjs.send("service_4f1u8bm", "template_zkhplpa", {
+      name: document.querySelector("#user_name").value,
+      message: document.querySelector("#message").value,
+      email: document.querySelector("#user_mail").value,
+    });
+
+    document.querySelector("#contact-form").classList.add("hidden");
+    document.querySelector("#succes-message").classList.remove("hidden");
+
+    const snack = document.querySelector("#snackbar");
+    snack.className = "show";
+    setTimeout(() => {
+      snack.className = snack.className.replace("show", "");
+    }, 5000);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
 
 (() => {
-	emailjs.init('user_tsBv3qPDJaxkkm7K9BsQi');
+  emailjs.init({
+    publicKey: "82LYrTyAJukkNcSym",
+    blockHeadless: true,
+  });
 
-	window.onload = () => {
-		document
-			.getElementById('contact-form')
-			.addEventListener('submit', (event) => {
-				event.preventDefault();
-				emailjs
-					.send('hiddevbavel_gmail_com', 'template_V9HUOUNS', {
-						from_name: document.querySelector('#user_name').value,
-						message: generateMessage(
-							document.querySelector('#message').value,
-							document.querySelector('#user_phone').value,
-						),
-						from_mail: document.querySelector('#user_mail').value,
-					})
-					.then(
-						(response) => {
-							document.querySelector('#contact-form').classList.add('hidden');
-							document
-								.querySelector('#succes-message')
-								.classList.remove('hidden');
-
-							const snack = document.querySelector('#snackbar');
-							snack.className = 'show';
-							setTimeout(() => {
-								snack.className = snack.className.replace('show', '');
-							}, 5000);
-						},
-						(error) => {
-							console.log('FAILED...', error);
-						},
-					);
-			});
-	};
+  window.onload = () => {
+    document
+      .getElementById("contact-form")
+      .addEventListener("submit", handleSubmit);
+  };
 })();
